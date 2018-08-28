a4262d3e132b909dd08f7761557375db1c4ee0d6
jamieberrier <berrier.jamie@gmail.com>
2018-08-28 00:22:01 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,27 +1,3 @@
-class Doctor
-  attr_accessor
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  # takes in a date and an instance of the Patient class and creates a new appointment.
-  # That appointment should know that it belongs to the doctor
-  def new_appointment
-    
-  end
-
-  # iterates over that doctor's appointments and collects the patient that belongs
-  # to each appointment.
-  def patients
-
-  end
+class Appointment
 
 end
