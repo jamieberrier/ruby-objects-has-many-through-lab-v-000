b728feb956841fee41dbb38573b6152ef44fe81d
jamieberrier <berrier.jamie@gmail.com>
2018-08-28 00:16:42 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,8 @@ class Doctor
 
   # takes in a date and an instance of the Patient class and creates a new appointment.
   # That appointment should know that it belongs to the doctor
-  def new_appointment(date, patient)
+  def new_appointment(date, patient) 
+    Appointment.new()
 
   end
 
