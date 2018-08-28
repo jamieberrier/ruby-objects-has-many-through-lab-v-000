120cab8d57f1930261925b3c028d913bd7124d6d
jamieberrier <berrier.jamie@gmail.com>
2018-08-28 00:44:54 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -7,7 +7,7 @@ class Appointment
 
   # initialized with a date (as a string, like "Monday, August 1st"), a patient, and
   # a doctor. The appointment should be saved in the @@all array.
-  def initialize(patient, date, doctor)
+  def initialize(date, patient, doctor)
     binding.pry
     @date = date
     @patient = patient
