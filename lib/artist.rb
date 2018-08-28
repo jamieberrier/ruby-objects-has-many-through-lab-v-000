b07a469d83d7215b863d8459ad8fba2d0913a4e7
jamieberrier <berrier.jamie@gmail.com>
2018-08-28 00:06:44 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    #binding.pry
+    song = Song.new(name, self, genre)
+  end
+
+  def songs
+
+  end
+
+  def genres
+
+  end
+end
