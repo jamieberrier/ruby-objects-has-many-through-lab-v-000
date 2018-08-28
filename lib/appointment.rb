2ed8bbc3e67e89d2e827f49a69d5695883e60b54
jamieberrier <berrier.jamie@gmail.com>
2018-08-28 00:27:04 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,11 +3,13 @@ class Appointment
 
   @@all = []
 
+  # initialized with a date (as a string, like "Monday, August 1st"), a patient, and a doctor. 
+  # The appointment should be saved in the @@all array.
   def initialize()
 
   end
 
-  def self.all 
+  def self.all
     @@all
   end
 end
